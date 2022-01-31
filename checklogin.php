6852f3034be4335c8aceb01cfbe3b5c7eacd56f4
Ming Wei Kwong <s10195172@connect.np.edu.sg>
2022-01-31 23:34:59 +0800

    checkLogin changed

--- checklogin.php
+++ checklogin.php
@@ -4,44 +4,44 @@ session_start();
 // Include the Page Layout header
 include("header.php"); 
 
+
+
 // Reading inputs entered in previous page
-$email = strtolower($_POST["email"]);
+$email = $_POST["email"];
 $pwd = $_POST["password"];
-include_once("mysql_conn.php");
+
 // To Do 1 (Practical 2): Validate login credentials with database
-$qry = "SELECT * FROM Shopper WHERE Email = ?";
-$stmt = $conn->prepare($qry);
-$stmt->bind_param("s", $email);
+include_once("mysql_conn.php");
+$qry="SELECT * from shopper WHERE Email=?";
+$stmt=$conn->prepare($qry);
+$stmt->bind_param("s",$email);
 $stmt->execute();
-$result = $stmt->get_result();
+$result=$stmt->get_result();
 $stmt->close();
-if ($result->num_rows > 0) {
-	// Save user's info in session variables
-	$row1 = $result->fetch_array();
-	$hashed_pwd = $row1["Password"];
-	if (password_verify($pwd,$hashed_pwd)==true){
-		$checkLogin = true;		
-		$_SESSION["ShopperName"] = $row1["Name"];
-		$_SESSION["ShopperID"] = $row1["ShopperID"];
-	}
-	
-	// To Do 2 (Practical 4): Get active shopping cart
-	$qry = "SELECT sc.ShopCartID, COUNT(sci.ProductID) 
-			AS NumItems FROM ShopCart sc 
-			LEFT JOIN ShopCartItem sci on sc.ShopCartID=sci.ShopCartID
-			WHERE sc.ShopperID=$_SESSION[ShopperID] AND sc.OrderPlaced=0";
-	$result2 = $conn->query($qry);
-	$row2 = $result2->fetch_array();
+$loggedIn=FALSE;
+if ($result->num_rows>0){
+	$row=$result->fetch_array();
+	$hashed_pwd=$row["Password"];
+		$_SESSION["ShopperName"] = $row["Name"];
+		$_SESSION["ShopperID"] = $row["ShopperID"];
+		$loggedIn=TRUE;
+		// To Do 2 (Practical 4): Get active shopping cart
+		$qry2="SELECT a.ShopCartID,SUM(b.Quantity) As ItemCount from ShopCart a LEFT JOIN ShopCartItem b ON a.ShopCartID=b.ShopCartID WHERE a.ShopperID=$_SESSION[ShopperID] AND a.OrderPlaced=0";
+		$result2=$conn->query($qry2);
+		$row2=$result2->fetch_array();
 	$_SESSION["Cart"] = $row2["ShopCartID"];
-	$_SESSION["NumCartItem"] = $row2["NumItems"];
+	$_SESSION["NumCartItem"] = $row2["ItemCount"];
+	$conn->close();
+}
+
+if ($loggedIn){
 	// Redirect to home page
 	header("Location: index.php");
 	exit;
 }
-else {
-	echo  "<h3 style='color:red'>Invalid Login Credentials</h3>";
+else{
+	echo "Invalid login credential";
 }
-$conn->close();
 // Include the Page Layout footer
 include("footer.php");
 ?>
