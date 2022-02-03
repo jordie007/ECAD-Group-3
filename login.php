7aae9480ccb66740635dc6215d17752d59d404cf
CaeNGnp <s10196427@connect.np.edu.sg>
2022-02-03 18:30:58 +0800

    update login flow

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 session_start();
 include("header.php");
 ?>
+
 <div style="width:80%;margin:auto">
     <form action="checkLogin.php" method="POST">
         <div class="form-group row">
@@ -12,20 +13,27 @@ include("header.php");
         <div class="form-group row">
             <label class="col-sm-3 col-form-label" for="email">Email Address:</label>
             <div class="col-sm-9">
-                <input class="form-control" type="email" name="email" id="email" required/>
+                <input class="form-control" type="email" name="email" id="email" required />
             </div>
         </div>
         <div class="form-group row">
             <label class="col-sm-3 col-form-label" for="password">Password:</label>
             <div class="col-sm-9">
-                <input class="form-control" type="password" name="password" id="password" required/>
+                <input class="form-control" type="password" name="password" id="password" required />
             </div>
         </div>
         <div class="form-group row">
+            <?php if ($_GET["error"] ?? false) { ?>
+                <div class="col-sm-9 offset-sm-3 text-danger pb-2">
+                    Invalid login credentials!
+                </div>
+            <?php } ?>
             <div class="col-sm-9 offset-sm-3">
                 <button type="submit" class="btn btn-primary">Login</button>
-                <p>Please sign up if you do not have an account.</p>
-                <p><a href="forgetPassword.php">Forget Password</a></p>
+                <div class="pt-3">
+                    <a href="register.php">Sign up</a> |
+                    <a href="forgetPassword.php">Forget Password</a>
+                </div>
             </div>
         </div>
     </form>
