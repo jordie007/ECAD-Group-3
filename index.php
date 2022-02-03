dfefb01ad8c039e66640e0c78803aeed323e3fd3
JaxChua <69745694+JaxChua@users.noreply.github.com>
2022-02-03 12:57:17 +0800

    Updated to fit theme

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ session_start();
 // Include the Page Layout header
 include("header.php"); 
 ?>
-<img src="Images/welcome2mamaya.jpg" class="img-fluid" 
-     style="display:block; margin:auto;"/>
-
+<div style="margin:auto;text-align:center;
+background-image: url('Images/welcome.jpg');
+background-repeat: no-repeat;
+background-position: center;min-height:50vh;">
+<p style="font-size: 5vw;
+    font-weight: 900;  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);width:100%"> Welcome!<br/>Take a bite out of hunger</p>
+</div>
 <?php 
 // Include the Page Layout footer
 include("footer.php"); 
