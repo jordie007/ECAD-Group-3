be8027f7eac734dc4416a5e5d7b46d61a174ac61
JaxChua <69745694+JaxChua@users.noreply.github.com>
2022-02-03 12:22:10 +0800

    Updated estimated delivery date

--- orderConfirmed.php
+++ orderConfirmed.php
@@ -22,7 +22,15 @@ if(isset($_SESSION["OrderID"]) and isset($_SESSION["delivery"]) and isset($_SESS
 	if($_SESSION["delivery"] == "Express"){
 		echo date("j/n/y g:i a", strtotime("+2 hour"));
 	}
-	else echo date("j/n/y g:i a", strtotime("+1 day"));
+	else {
+		if (in_array(date("l"), ["Friday"])){
+			echo date("j/n/y g:i a", strtotime("+3 day"));
+		}
+		elseif (in_array(date("l"), ["Saturday"])){
+			echo date("j/n/y g:i a", strtotime("+2 day"));
+		}
+		else echo date("j/n/y g:i a", strtotime("+1 day"));
+	}
 	echo "<br>Grand Total: $$_SESSION[total]</p>";
 	echo "<p>Thank you for your purchase.&nbsp;&nbsp;";
 	echo '<a href="index.php">Continue shopping</a></p>';
