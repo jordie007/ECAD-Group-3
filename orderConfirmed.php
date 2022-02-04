dcc2c055b99cfe58c0cb47381a3377047ed12f07
JaxChua <69745694+JaxChua@users.noreply.github.com>
2022-02-04 16:05:23 +0800

    Design for confirmed order

--- orderConfirmed.php
+++ orderConfirmed.php
@@ -4,8 +4,12 @@ include("header.php"); // Include the Page Layout header
 
 if(isset($_SESSION["OrderID"]) and isset($_SESSION["delivery"]) and isset($_SESSION["total"])) {	
 	include_once("mysql_conn.php");
-	echo "<p>Checkout successful. Your order number is $_SESSION[OrderID]</p>";
-	echo "<p><b>Order Summary</b><br>";
+	echo "<div class='col-lg-6' style='margin:auto;padding:0;border-radius:25px;box-shadow: 0px 0px 10px #C6C6C6;'>";
+	echo "<div style='font-weight:bold;background-color:#ffbb00;padding:20px;
+	border-top-left-radius:25px;border-top-right-radius:25px;text-align:center'>
+	<p style='color:green;font-size:2em'>Checkout successful</p><p> Order Number: <b>$_SESSION[OrderID]</b></p></div>";
+	echo "<div style='background-color:white;padding:20px;
+	border-bottom-left-radius:25px;border-bottom-right-radius:25px;'><p style='text-align:center'><b style='font-size:1.25em;margin:0'>Order Summary</b><br>";
 	$qry = "SELECT sci.Name, sci.Quantity FROM shopcartitem sci 
 			INNER JOIN shopcart sc ON sci.ShopCartID = sc.ShopCartID 
 			INNER JOIN orderdata od ON od.ShopCartID=sc.ShopCartID 
@@ -13,12 +17,17 @@ if(isset($_SESSION["OrderID"]) and isset($_SESSION["delivery"]) and isset($_SESS
 	$result = $conn->query($qry);
 	if ($result->num_rows > 0){
 		while ($row = $result->fetch_array()){
-			echo "$row[Name] x $row[Quantity]<br>";
+			$qry1 = "SELECT ProductImage FROM Product WHERE ProductTitle = '$row[Name]'";
+			$result1 = $conn->query($qry1);
+			$row1 = $result1->fetch_array();
+			echo "<div class='row'><div class='col-lg-8 d-flex' style='margin:auto'>
+			<div><img src='Images/Products/$row1[ProductImage]' style='width:10vh'></div>
+			<div class='m-lg-auto my-auto'>$row[Name] x $row[Quantity]</div></div></div>";
 		}
 	}
-	echo "Delivery Mode: $_SESSION[delivery]<br>";
+	echo "<hr style='border-color:#5c4300; width:100%;'>Delivery Mode: <b style='float:right'>$_SESSION[delivery]</b><br>";
 	date_default_timezone_set('Asia/Singapore');
-	echo "Estimated Delivery Date and time: ";
+	echo "Estimated Delivery Date and time: <b style='float:right'>";
 	if($_SESSION["delivery"] == "Express"){
 		echo date("j/n/y g:i a", strtotime("+2 hour"));
 	}
@@ -31,9 +40,10 @@ if(isset($_SESSION["OrderID"]) and isset($_SESSION["delivery"]) and isset($_SESS
 		}
 		else echo date("j/n/y g:i a", strtotime("+1 day"));
 	}
-	echo "<br>Grand Total: $$_SESSION[total]</p>";
+	echo "</b><br>Grand Total: <b style='float:right'>$$_SESSION[total]</b></p> <hr style='border-color:#5c4300; width:100%;'>";
 	echo "<p>Thank you for your purchase.&nbsp;&nbsp;";
-	echo '<a href="index.php">Continue shopping</a></p>';
+	echo '<a href="index.php" style="background-color:#ffbb00;text-decoration:none;font-weight:bold;float:right;color:black;padding:10px;border-radius:10px">Continue shopping</a></p>';
+	echo "</div></div>";
 } 
 else header("Location: index.php");
 
