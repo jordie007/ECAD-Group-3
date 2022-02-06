c277de05c76d1444ee17a757f412ddf1adbf4512
CaeNGnp <s10196427@connect.np.edu.sg>
2022-02-06 16:26:14 +0800

    catProduct: bad id check

--- catalogue/catProduct.php
+++ catalogue/catProduct.php
@@ -1,37 +1,51 @@
 <?php
 session_start(); // STart the current session
+
+$baseURI = ".."; // override; base dir is in parent dir
+
 // Include the PHP file that establishes database connection handle: $conn
 include_once("../mysql_conn.php");
+include("../header.php"); // Include the Page Layout header
 include_once("ProductCatalogue.php");
 
+// Print product catalogue component
 ProductCatalog::echoStyle();
 ?>
 
 <?php
-$baseURI = ".."; // override; base dir is in parent dir
-include("../header.php"); // Include the Page Layout header
+
+
+$cid = $_GET["cid"] ?? null;
+
+$qry = "SELECT * FROM Category WHERE CategoryID = ?";
+$stmt = $conn->prepare($qry);
+$stmt->bind_param("i", $cid);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+
+$rowCat = $result->fetch_assoc();
+if (!$rowCat) {
+?>
+  <h4 class="text-danger">No category with the ID <?= $cid ?> was found.</h4>
+<?php
+  exit;
+}
+
+
 ?>
 
 <div class="container">
   <!-- Display Page Header - Category's name is queried from the DB -->
   <div class="row px-3">
     <span class="page-title">
-      Category – <?php
-                  $qry = "SELECT * FROM Category WHERE CategoryID = ?";
-                  $stmt = $conn->prepare($qry);
-                  $stmt->bind_param("i", $_GET["cid"]);
-                  $stmt->execute();
-                  $result = $stmt->get_result();
-                  $stmt->close();
-
-                  echo $result->fetch_assoc()["CatName"];
-                  ?>
+      Category – <?= $rowCat["CatName"]; ?>
     </span>
   </div>
 
   <?php
 
-  $cid = $_GET["cid"];
+
   $qry = "SELECT *
 		FROM CatProduct cp INNER JOIN Product p ON cp.ProductID = p.ProductID
 		WHERE cp.CategoryID=?
